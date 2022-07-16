678649270d73b88761dc5f7e52f4cd7af71b7105
Daniel Flaws <dflaws1@gmail.com>
2022-07-16 14:51:59 -0400

    Add card images to server

---
+++ HAF.WebServer/wwwroot/images/index.ts
@@ -0,0 +1,111 @@
+const clubs_2 = require('./2_of_clubs.tsx') as string;
+// import * as diamonds_2 from './2_of_diamonds.svg'
+// import * as hearts_2 from './2_of_hearts.svg'
+// import * as spades_2 from './2_of_spades.svg'
+// import * as clubs_3 from './3_of_clubs.svg'
+// import * as diamonds_3 from './3_of_diamonds.svg'
+// import * as hearts_3 from './3_of_hearts.svg'
+// import * as spades_3 from './3_of_spades.svg'
+// import * as clubs_4 from './4_of_clubs.svg'
+// import * as diamonds_4 from './4_of_diamonds.svg'
+// import * as hearts_4 from './4_of_hearts.svg'
+// import * as spades_4 from './4_of_spades.svg'
+// import * as clubs_5 from './5_of_clubs.svg'
+// import * as diamonds_5 from './5_of_diamonds.svg'
+// import * as hearts_5 from './5_of_hearts.svg'
+// import * as spades_5 from './5_of_spades.svg'
+// import * as clubs_6 from './6_of_clubs.svg'
+// import * as diamonds_6 from './6_of_diamonds.svg'
+// import * as hearts_6 from './6_of_hearts.svg'
+// import * as spades_6 from './6_of_spades.svg'
+// import * as clubs_7 from './7_of_clubs.svg'
+// import * as diamonds_7 from './7_of_diamonds.svg'
+// import * as hearts_7 from './7_of_hearts.svg'
+// import * as spades_7 from './7_of_spades.svg'
+// import * as clubs_8 from './8_of_clubs.svg'
+// import * as diamonds_8 from './8_of_diamonds.svg'
+// import * as hearts_8 from './8_of_hearts.svg'
+// import * as spades_8 from './8_of_spades.svg'
+// import * as clubs_9 from './9_of_clubs.svg'
+// import * as diamonds_9 from './9_of_diamonds.svg'
+// import * as hearts_9 from './9_of_hearts.svg'
+// import * as spades_9 from './9_of_spades.svg'
+// import * as clubs_10 from './10_of_clubs.svg'
+// import * as diamonds_10 from './10_of_diamonds.svg'
+// import * as hearts_10 from './10_of_hearts.svg'
+// import * as spades_10 from './10_of_spades.svg'
+// import * as clubs_ace from './ace_of_clubs.svg'
+// import * as diamonds_ace from './ace_of_diamonds.svg'
+// import * as hearts_ace from './ace_of_hearts.svg'
+// import * as spades_ace from './ace_of_spades.svg'
+// import * as clubs_jack from './jack_of_clubs.svg'
+// import * as diamonds_jack from './jack_of_diamonds.svg'
+// import * as hearts_jack from './jack_of_hearts.svg'
+// import * as spades_jack from './jack_of_spades.svg'
+// import * as clubs_king from './king_of_clubs.svg'
+// import * as diamonds_king from './king_of_diamonds.svg'
+// import * as hearts_king from './king_of_hearts.svg'
+// import * as spades_king from './king_of_spades.svg'
+// import * as clubs_queen from './queen_of_clubs.svg'
+// import * as diamonds_queen from './queen_of_diamonds.svg'
+// import * as hearts_queen from './queen_of_hearts.svg'
+// import * as spades_queen from './queen_of_spades.svg'
+// import * as joker from './red_joker.svg'
+
+const images = {
+  clubs_2,
+  // diamonds_2,
+  // hearts_2,
+  // spades_2,
+  // clubs_3,
+  // diamonds_3,
+  // hearts_3,
+  // spades_3,
+  // clubs_4,
+  // diamonds_4,
+  // hearts_4,
+  // spades_4,
+  // clubs_5,
+  // diamonds_5,
+  // hearts_5,
+  // spades_5,
+  // clubs_6,
+  // diamonds_6,
+  // hearts_6,
+  // spades_6,
+  // clubs_7,
+  // diamonds_7,
+  // hearts_7,
+  // spades_7,
+  // clubs_8,
+  // diamonds_8,
+  // hearts_8,
+  // spades_8,
+  // clubs_9,
+  // diamonds_9,
+  // hearts_9,
+  // spades_9,
+  // clubs_10,
+  // diamonds_10,
+  // hearts_10,
+  // spades_10,
+  // clubs_ace,
+  // diamonds_ace,
+  // hearts_ace,
+  // spades_ace,
+  // clubs_jack,
+  // diamonds_jack,
+  // hearts_jack,
+  // spades_jack,
+  // clubs_king,
+  // diamonds_king,
+  // hearts_king,
+  // spades_king,
+  // clubs_queen,
+  // diamonds_queen,
+  // hearts_queen,
+  // spades_queen,
+  // joker
+}
+
+export default images;
